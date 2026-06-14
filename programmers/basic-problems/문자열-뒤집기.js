732b347754030d91b0637977340bdc3b1a3af11a
JGW-Korea <jgw6372@gmail.com>
2026-06-15 06:40:18 +0900

    programmers: 문자열 뒤집기 문제 별해 추가

--- programmers/basic-problems/문자열-뒤집기.js
+++ programmers/basic-problems/문자열-뒤집기.js
@@ -1,4 +1,4 @@
-function solution(my_string, s, e) {
+function solutionA(my_string, s, e) {
   const answer = [ ...my_string.split("") ];
 
   let p1 = s;
@@ -9,6 +9,18 @@ function solution(my_string, s, e) {
 
     p1 += 1;
     p2 -= 1;
+  }
+
+  return answer.join("");
+}
+
+function solutionB(my_string, s, e) {
+  const answer = [ ...my_string.split("") ];
+
+  for(let i = s; i < Math.floor((s + e) / 2) + 1; i++) {
+    let temp = answer[i];
+    answer[i] = answer[(s + e) - i];
+    answer[(s + e) - i] = temp;
   }
 
   return answer.join("");
